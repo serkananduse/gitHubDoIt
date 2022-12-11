e5c7796bd464c67c2db5d549fa33d81dc1e14b2c
serkananduse <fatser2701@gmail.com>
2022-12-11 23:05:07 +0100

    1

--- gitExercisse/src/newPackage/GitHubDoIt.java
+++ gitExercisse/src/newPackage/GitHubDoIt.java
@@ -1,7 +1,22 @@
 package newPackage;
 
+import java.sql.Array;
+import java.util.Arrays;
+
 public class GitHubDoIt {
     public static void main(String[] args) {
         System.out.println("hello World");
+        String str ="uiefhskdfjksdfjpowi3erjlkdj02349523485739453";
+       Arrays.toString(str.split(""));
+       String klm ="sdsdf343";
+       String [] arklm =klm.split("");
+        System.out.println(arklm[0]);
+        System.out.println(Arrays.toString(arklm));
+        System.out.println(Arrays.toString(arklm));
+        String [] abc ="sevmebeni".split("e");
+        System.out.println(Arrays.toString(abc));
+
+
     }
-}
+    }
+
